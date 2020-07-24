a2925abb9654c0fe889af82bc1282f3b6e23dac1
lyh543 <lxl361429916@live.com>
2020-07-24 14:00:55 +0800

    添加转发功能

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -18,3 +18,43 @@ App({
     this.globalData = {}
   }
 })
+
+/**
+ * 全局分享配置，页面无需开启分享
+ * 使用隐式页面函数进行页面分享配置
+ * 使用隐式路由获取当前页面路由，并根据路由来进行全局分享、自定义分享
+ * 来源：https://www.cnblogs.com/xyyt/p/12614181.html
+ */
+! function () {
+  //获取页面配置并进行页面分享配置
+  var PageTmp = Page
+  Page = function (pageConfig) {
+    //1. 获取当前页面路由
+    let routerUrl = ""
+    wx.onAppRoute(function (res) {
+      //app.js中需要在隐式路由中才能用getCurrentPages（）获取到页面路由
+      let pages = getCurrentPages(),
+        view = pages[pages.length - 1];
+      routerUrl = view.route
+    })
+
+    //2. 全局开启分享配置
+    pageConfig = Object.assign({
+      onShareAppMessage: function () {
+        //根据不同路由设置不同分享内容（微信小程序分享自带参数，如非特例，不需配置分享路径）
+        let shareInfo={}
+        let noGlobalSharePages=["index/index"]
+        //全局分享配置，如部分页面需要页面默认分享或自定义分享可以单独判断处理
+        if (!routerUrl.includes(noGlobalSharePages)){
+          shareInfo = {
+            title: "阮薇薇点名啦",
+            imageUrl: wx.getStorageSync("shareUrl")
+          }
+        }
+        return shareInfo
+      }
+    }, pageConfig);
+    // 配置页面模板
+    PageTmp(pageConfig);
+  }
+}();
--- miniprogram/pages/init_user/init_user.js
+++ miniprogram/pages/init_user/init_user.js
@@ -57,10 +57,4 @@ Page({
 
   },
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
