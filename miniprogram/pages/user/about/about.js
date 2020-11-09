35d3bc7e6343a015cff1221bfd5feb8907270768
lyh543 <lyh543@outlook.com>
2020-11-09 12:29:25 +0800

    “我也不知道添加了什么”

--- miniprogram/pages/user/about/about.js
+++ miniprogram/pages/user/about/about.js
@@ -14,4 +14,20 @@ Page({
     var link = e.currentTarget.dataset.link;
     wx.setClipboardData({data: link});
   },
+  clickPicture() {
+    if (this.startTap == undefined || Date.now() - this.startTap > 2000) { // 第一次点击或距离第一次点击已经超过 2s，则重置
+      this.startTap = Date.now();
+      this.tapTime = 0;
+    } else {
+      this.tapTime++;
+      if (this.tapTime == 5) {
+        this.testMode = true;
+        this.tapTime = 0;
+        wx.showToast({
+          title: 'tljj 女装赛高！',
+          icon: 'none'
+        })
+      }
+    }
+  }
 })
